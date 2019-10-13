9151d8a1018f0355fe8f883cea0d45c7ba05a7c9
Lee Irvine <lee@Unknown.wavecable.com>
2019-10-13 16:36:30 -0700

    wads/doom2.wad wads/brutalv21.pk3 wads/e-day.pk3 wads/golf.pk3 wads/baldi.pk3

--- join-game-windows.bat
+++ join-game-windows.bat
@@ -1,2 +1,2 @@
 git pull https master
-.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/vacation-doom.wad wads/monster-doom.wad
+.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/brutalv21.pk3 wads/e-day.pk3 wads/golf.pk3 wads/baldi.pk3

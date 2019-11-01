c74725640ca1a7c4458ca77d68f138d0c08caf68
Lee Irvine <lee@rawr.wavecable.com>
2019-10-31 18:43:11 -0700

    wads/doom2.wad wads/brutalv21.pk3 wads/spy-doom.wad wads/golf.pk3

--- join-game-windows.bat
+++ join-game-windows.bat
@@ -1,2 +1,2 @@
 git pull https master
-.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/project-brutality-2.0.3.pk3 wads/pumpkin-hell.wad
+.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/brutalv21.pk3 wads/spy-doom.wad wads/golf.pk3

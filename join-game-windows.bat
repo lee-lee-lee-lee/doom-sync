be8fc7b46f8d410d3c0cb6a5de18d1655aeb8808
Lee Irvine <lee@rawr.wavecable.com>
2019-10-31 18:02:13 -0700

    wads/doom2.wad wads/project-brutality-2.0.3.pk3 wads/pumpkin-hell.wad

--- join-game-windows.bat
+++ join-game-windows.bat
@@ -1,2 +1,2 @@
 git pull https master
-.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/brutalv21.pk3 wads/e-day.pk3 wads/baldi.wad
+.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/project-brutality-2.0.3.pk3 wads/pumpkin-hell.wad

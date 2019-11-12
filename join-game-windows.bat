507d101f63bc03e399cde5b670b2d46926cd561c
Lee Irvine <lee@rawr.wavecable.com>
2019-11-11 20:50:27 -0800

    wads/doom2.wad wads/brutalv21.pk3 wads/vacation-doom.wad wads/sonic-doom.pk3

--- join-game-windows.bat
+++ join-game-windows.bat
@@ -1,2 +1,2 @@
 git pull https master
-.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/brutalv21.pk3 wads/sonic-doom.pk3 wads/vacation-doom.wad
+.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/brutalv21.pk3 wads/vacation-doom.wad wads/sonic-doom.pk3

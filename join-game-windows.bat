b3555ab12a90393828df455536ba3b5d3d84d973
Lee Irvine <lee@rawr.wavecable.com>
2019-12-02 20:10:10 -0800

    wads/doom2.wad wads/ghostbuster-doom.wad

--- join-game-windows.bat
+++ join-game-windows.bat
@@ -1,2 +1,2 @@
 git pull https master
-.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/ghostbusters-doom.wad
+.\bin\zandronum.exe -iwad wads/doom2.wad -connect 24.56.254.30 -file wads/ghostbuster-doom.wad
